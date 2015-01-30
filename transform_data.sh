50948e6a19a460879601c839d57785c30265ebea
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-01-30 16:59:29 +0100

    Don't recreate file if already exists

--- transform_data.sh
+++ transform_data.sh
@@ -4,7 +4,10 @@ for i in `ls data/*.shp`
 do
     file=$(basename $i .shp)
     echo Transforming $file
-    ogr2ogr -t_srs 'LOCAL_CS["foo", EXTENSION["PROJ4", "+proj=natearth +wktext"]]' -overwrite /tmp/$file.shp $i
+    if ![ -a "/tmp/$file.shp" ]
+    then
+        ogr2ogr -t_srs 'LOCAL_CS["foo", EXTENSION["PROJ4", "+proj=natearth +wktext"]]' -overwrite /tmp/$file.shp $i
+    fi
 done
 
 for i in `ls layers/*/*.shp`
